16a8f12f3a84c78aacc08ad685369d15c748e9ed
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-05-13 16:12:59 +0200

    Prima bozza header (fatto a mano)

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,72 @@
+<template>
+
+  <header>
+
+    <div>
+      <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+
+    <ul>
+      <li><a href="#">characters</a></li>
+      <li><a class="active" href="#">comics</a></li>
+      <li><a href="#">movies</a></li>
+      <li><a href="#">tv</a></li>
+      <li><a href="#">games</a></li>
+      <li><a href="#">collectibles</a></li>
+      <li><a href="#">videos</a></li>
+      <li><a href="#">fans</a></li>
+      <li><a href="#">news</a></li>
+      <li><a href="#">shop</a></li>
+    </ul>
+  </header>
+  
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "../assets/style/variable.scss";
+
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100px;
+  border: 1px solid black;
+  font-size: 14px;
+
+  ul{
+    display: flex;
+  }
+
+  li{
+    list-style: none;
+    margin-right: 30px;
+
+    a{
+      text-transform: uppercase;
+      color: black;
+      text-decoration: none;
+      padding: 35px 0;
+
+      &:hover{
+        color: $primary-color;
+      }
+
+      &.active{
+        border-bottom: 6px solid $primary-color;
+      }
+
+    }
+
+
+  }
+
+}
+
+</style>
