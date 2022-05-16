f6766481bcdc27d3bee1e2970e8bff73711d7ec8
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-05-16 13:12:58 +0100

    Esercizio completato

---
+++ src/components/comicsCard.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="card-element">
+        <img :src="card.thumb" alt=""><br>
+        <span>{{card.series}}</span>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name:"ComicsCard",
+  props:{
+        card: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.card-element{
+
+        width: calc((100% / 6) - 20px);
+        margin: 0px 10px;
+        margin-bottom: 40px;
+
+        img{
+          margin-bottom: 10px;
+          width: 100%;
+
+        }
+        
+        span{
+          text-transform: uppercase;
+        }
+
+      }
+
+</style>
