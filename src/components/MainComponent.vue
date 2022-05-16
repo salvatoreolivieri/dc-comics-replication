def3a269422524fb52d8174ea33b1c35355af1ef
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-05-16 12:36:48 +0100

    Impostata struttura layout

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,51 @@
 <template>
 
   <main>
-    <h1>Content goes here</h1> 
+
+    <section class="hero-section">
+      <button>current series</button>
+    </section>
+
+    <section>
+
+      <div class="card-section">
+
+        <div class="card-element">
+          <img src="../assets/img/1.jpg" alt=""><br>
+          <span>ciao</span>
+        </div>
+        <div class="card-element">
+          <img src="../assets/img/1.jpg" alt=""><br>
+          <span>ciao</span>
+        </div>
+        <div class="card-element">
+          <img src="../assets/img/1.jpg" alt=""><br>
+          <span>ciao</span>
+        </div>
+        <div class="card-element">
+          <img src="../assets/img/1.jpg" alt=""><br>
+          <span>ciao</span>
+        </div>
+        <div class="card-element">
+          <img src="../assets/img/1.jpg" alt=""><br>
+          <span>ciao</span>
+        </div>
+        <div class="card-element">
+          <img src="../assets/img/1.jpg" alt=""><br>
+          <span>ciao</span>
+        </div>
+        <div class="card-element">
+          <img src="../assets/img/1.jpg" alt=""><br>
+          <span>ciao</span>
+        </div>
+      </div>
+
+      <div class="load-more-button-container">
+        <button class="load-more-button">load more</button>
+      </div>
+
+    </section>
+
   </main>
   
 </template>
@@ -14,16 +58,91 @@ export default {
 
 <style lang="scss" scoped>
 
+@import "../assets/style/variable";
+
   main{
     // width: 80%;
     margin: 0 auto;
-
-    height: 150px;
-    background-color: #2a2a2a;
+    background-color: #1C1C1C;
     color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+
+    .hero-section{
+      background-image: url("../assets/img/jumbotron.jpg");
+      background-size: cover;
+      height: 400px;
+      width: 100%;
+      position: relative;
+
+      button{
+        position: absolute;
+        bottom: -24px;
+        left: 217px;
+
+        background-color: $primary-color;
+        border: none;
+        padding: 15px;
+        text-transform: uppercase;
+        color: white;
+        font-weight: bold;
+        font-size: 16px;
+
+      }
+
+    }
+
+    .card-section{
+      width: 70%;
+      margin: 0 auto;
+      border: 1px solid white;
+      padding: 45px 10px;
+      display: flex;
+      flex-wrap: wrap;
+
+
+      .card-element{
+
+        width: calc((100% / 6) - 20px);
+        margin: 0px 10px;
+        margin-bottom: 40px;
+
+        img{
+          margin-bottom: 10px;
+          width: 100%;
+
+        }
+        
+        span{
+          text-transform: uppercase;
+        }
+
+      }
+
+
+    }
+
+    .load-more-button-container{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 70px;
+
+
+      .load-more-button{
+        background-color: $primary-color;
+        border: none;
+        padding: 10px 45px;
+        color: white;
+        text-transform: uppercase;
+        font-size: 11px;
+        font-weight: bold;
+
+        cursor: pointer;
+      }
+
+    }
+
+
+
   }
 
 </style>
